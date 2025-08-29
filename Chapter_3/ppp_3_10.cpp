91351bbf3268839e61c762bb212d75d395c5ac18
Felipe Artioli <feartioli.br@gmail.com>
2025-08-29 17:57:36 -0300

    Add ppp_3_10
    Exercise 3 from chapter 10. Used unsigned long long to get to the answer

---
+++ Chapter_3/ppp_3_10.cpp
@@ -0,0 +1,26 @@
+// PPP - Chapter 3 - Exercise 10
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  unsigned long long grains_on_square{1};
+  unsigned long long total_grains{};
+  constexpr int board_size{64};
+
+  cout << "Square:" << "\t\t" << "Gifted grains:" << '\n';
+
+  for (int i = 1; i <= board_size; ++i) {
+    total_grains += grains_on_square;
+    cout << i << "\t\t" << total_grains << "\n";
+    grains_on_square *= 2;
+  }
+}
+
+// Try to calculate the number of rice grains that the inventor asked for in
+// exercise 9 above. You’ll find that the number is so large that it won’t fit
+// in an int or a double. Observe what happens when the number gets too
+// large to represent exactly as an int and as a double. What is the largest
+// number of squares for which you can calculate the exact number of
+// grains (using an int)? What is the largest number of squares for which
+// you can calculate the approximate number of grains (using a double)
