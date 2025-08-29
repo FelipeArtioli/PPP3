73f85da40b73b60a711370ee257e492d907b0bab
Felipe Artioli <feartioli.br@gmail.com>
2025-08-29 11:39:57 -0300

    Added ppp_1_0
    Added drill from chapter 1. I'll skip adding the exercises for this
    chapter, since they are mostly outside of programming environment or
    variations of this drill.

---
+++ Chapter_1/ppp_1_0.cpp
@@ -0,0 +1,15 @@
+// PPP - Chapter # - Exercise #
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Hello, World!" << '\n';
+
+}
+
+// Compile and run the “Hello, World!” program. An IDE will have a
+// compile-and-run button. Even something as simple as “Hello,
+// World!” very rarely compiles and runs in a first attempt to use a new
+// programming language or a new programming environment.
