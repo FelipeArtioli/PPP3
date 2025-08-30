9015937fb2e4e0e905bcea4113fbcb5b1c554f63
Felipe Artioli <feartioli.br@gmail.com>
2025-08-29 21:55:07 -0300

    Fixed ppp_3_11
    Now using a switch in the main loop and a vector to store the machine
    turns as the exercise proposes

--- Chapter_3/ppp_3_11.cpp
+++ Chapter_3/ppp_3_11.cpp
@@ -1,25 +1,17 @@
 // PPP - Chapter 3 - Exercise 11
 
 #include <iostream>
+#include <vector>
 using namespace std;
-
-char machine_turn(int a, int b, int c) {
-  if (a < b)
-    return 'r';
-  if (b > a)
-    return 'p';
-  if (c < a)
-    return 's';
-  return 'p';
-}
 
 string game_logic(char m, char p) {
   if (m == p)
     return "TIE!";
-  if ((m == 'r' and p == 's') or (m == 'p' and p == 'r') or
-      (m == 's' and p == 'p'))
+  else if ((m == 'r' and p == 's') or (m == 'p' and p == 'r') or
+           (m == 's' and p == 'p'))
     return "YOU LOSE!";
-  return "YOU WON!";
+  else
+    return "YOU WON!";
 }
 
 string char_to_string(char c) {
@@ -31,30 +23,47 @@ string char_to_string(char c) {
   case 's':
     return "SCISSORS";
   default:
-    return 0;
+    return "INVALID";
   }
 }
 
 int main() {
+  vector<char> machine_moves = {'r', 'p', 'r', 's', 's', 'p', 'r', 'r',
+                                'p', 'p', 'r', 's', 'p', 's', 'p', 'r',
+                                'r', 's', 'p', 'r', 's', 'p'};
 
-  char player_value{};
-  string player_string{};
-  char machine_value{};
-  string machine_string{};
-  int value1{}, value2{}, value3{};
-  string game_result{};
+  cout << "\nEnter rock, paper or scissors (r,p,s) or 'q' to quit:\n";
 
-  cout << "Enter 3 values: \n";
-  cin >> value1 >> value2 >> value3;
-  machine_value = machine_turn(value1, value2, value3);
-  cout << "\nEnter rock, paper or scissors (r,p,s): \n";
-  cin >> player_value;
-  game_result = game_logic(machine_value, player_value);
-  machine_string = char_to_string(machine_value);
-  player_string = char_to_string(player_value);
-  cout << "Machine played " << machine_string
-       << " and you played " << player_string << '\n';
-  cout << game_result << '\n';
+  int i{};
+  while (true) {
+    char player_value{};
+    cin >> player_value;
+
+    if (player_value == 'q')
+      break;
+
+    switch (player_value) {
+    case 'r':
+    case 'p':
+    case 's': {
+      char machine_value = machine_moves[i];
+      string machine_string = char_to_string(machine_value);
+      string player_string = char_to_string(player_value);
+      string result = game_logic(machine_value, player_value);
+
+      cout << "Machine played " << machine_string << " and you played "
+           << player_string << '\n';
+      cout << result << '\n';
+      break;
+    }
+    default:
+      cout << "Invalid input. Please enter 'r', 'p', or 's'.\n";
+    }
+
+    i = (i + 1) % machine_moves.size();
+  }
+
+  cout << "Thanks for playing!\n";
 }
 
 // Write a program that plays the game “Rock, Paper, Scissors.” Use a switch-
