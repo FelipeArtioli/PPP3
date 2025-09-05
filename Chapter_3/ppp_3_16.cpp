df6a513383f49c232354764fe34e6f73c10a5339
Felipe Artioli <feartioli.br@gmail.com>
2025-09-05 05:07:00 -0300

    Add ppp_3_16.cpp
    Add exercise 16 of chapter 3.

---
+++ Chapter_3/ppp_3_16.cpp
@@ -0,0 +1,62 @@
+// PPP - Chapter 3 - 16
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> sort_words(vector<string> words) {
+  sort(words.begin(), words.end());
+  return words;
+}
+
+string find_mode(vector<string> words) {
+  int curr_count{1}, max_count{1};
+  string mode{};
+  for (int i = 1; i < words.size(); ++i) {
+    if (words[i] == words[i - 1]) {
+      ++curr_count;
+      if (curr_count > max_count) {
+        max_count = curr_count;
+        mode = words[i];
+      }
+    } else {
+      curr_count = 1;
+    }
+  }
+  if (max_count == 1)
+    return ""; // "no mode"
+  else
+    return mode;
+}
+
+int main() {
+  vector<string> words = {};
+  string input{};
+  cout << "Enter a set of strings to find the min, max and mode of the set:\n";
+
+  while (cin >> input) {
+    words.push_back(input);
+  }
+
+  if (words.empty()) {
+    cout << "No words entered.\n";
+    return 0;
+  }
+
+  words = sort_words(words);
+  string mode = find_mode(words);
+  string min = words[0];
+  string max = words[words.size() - 1];
+
+  cout << "Max = " << max << '\n';
+  cout << "Min = " << min << '\n';
+  if (mode.empty())
+    cout << "No mode found (all words are unique).\n";
+  else
+    cout << "Mode = " << mode << '\n';
+}
+
+// Write a program that finds the min, max, and mode of a sequence of
+// strings.
