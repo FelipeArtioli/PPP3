3de5b253c42c792c2287b139660c18e5db00acb9
Felipe Artioli <feartioli.br@gmail.com>
2025-09-09 15:37:38 -0300

    Add ppp_4_03.cpp
    Add exercise 3 of Chapter 4

---
+++ Chapter_4/ppp_4_03.cpp
@@ -0,0 +1,30 @@
+// PPP - Chapter 4 - 3
+
+#include <iostream>
+using namespace std;
+
+double ctok(double c) {
+  double k = c + 273.25;
+  return k;
+}
+// converts Celsius to Kelvin
+int main() {
+  double c = 0;
+  cin >> c;
+  if (c <= -273.15) {
+    cerr << "ERROR: Temperature is below -273.15C, or 0K.\n";
+    return 1;
+  }
+  double k = ctok(c);
+  // declare input variable
+  // retrieve temperature to input variable
+  // convert temperature
+  cout << k << '\n';
+  // print out temperature
+}
+
+// Absolute zero is the lowest temperature that can be reached; it is
+// -273.15C, or 0K. The above program, even when corrected, will
+// produce erroneous results when given a temperature below this. Place a
+// check in the main program that will produce an error if a temperature is
+// given below -273.15C.
