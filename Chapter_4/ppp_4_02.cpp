19267c93cb0f106cf5e13796696545191e511621
Felipe Artioli <feartioli.br@gmail.com>
2025-09-09 15:29:41 -0300

    Add ppp_4_02.cpp
    Add exercise 2 of Chapter 4

---
+++ Chapter_4/ppp_4_02.cpp
@@ -0,0 +1,24 @@
+// PPP - Chapter 4 - 2
+
+#include <iostream>
+using namespace std;
+
+double ctok(double c) {
+  double k = c + 273.25;
+  return k;
+}
+// converts Celsius to Kelvin
+int main() {
+  double c = 0;
+  cin >> c;
+  double k = ctok(c);
+  // declare input variable
+  // retrieve temperature to input variable
+  // convert temperature
+  cout << k << '\n';
+  // print out temperature
+}
+
+// The following program takes in a temperature value in Celsius and
+// converts it to Kelvin. This code has many errors in it. Find the errors,
+// list them, and correct the code.
