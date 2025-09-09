482f0765810cbad720eb7073cb10c56b99ee4a6d
Felipe Artioli <feartioli.br@gmail.com>
2025-09-09 20:22:04 -0300

    Commented ppp_1_00.cpp

--- Chapter_1/ppp_1_00.cpp
+++ Chapter_1/ppp_1_00.cpp
@@ -1,13 +1,20 @@
 // PPP - Chapter 1 - Exercise 0
 
 #include <iostream>
+// This tells the compiler to include the input/output stream library.
 using namespace std;
+// This lets us use standard library features like 'cout' without having to
+// write 'std::cout' every time. I've heard this is a bad practice, but I'll use
+// this to keep the style similar to the book.
 
-int main(){
+int main() {
+  // 'main' is the entry point of every C++ program.
+  // The program starts running from here.
 
-    cout << "Hello, World!" << '\n';
-
+  cout << "Hello, World!" << '\n';
+  // 'cout' stands for "character output". It prints text to the screen.
 }
+// When the program finishes running 'main', it ends.
 
 // Compile and run the “Hello, World!” program. An IDE will have a
 // compile-and-run button. Even something as simple as “Hello,
