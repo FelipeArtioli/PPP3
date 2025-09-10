c524f5deedd2ae8989d2351cdac691e5764b3dfa
Felipe Artioli <feartioli.br@gmail.com>
2025-09-10 16:25:59 -0300

    Add ppp_4_06.cpp
    Add exercise 6 of chapter 4.

--- Chapter_4/ppp_4_06.cpp
+++ Chapter_4/ppp_4_06.cpp
@@ -1,22 +1,22 @@
-// PPP - Chapter 4 - 5
+// PPP - Chapter 4 - 6
 
 #include <iostream>
 using namespace std;
 
-double ctok(double c) {
+double ctof(double c) {
 
   if (c < -273.15)
     throw runtime_error("Temperature is below -273.15 C");
 
-  return c + 273.15;
+  return c * 1.8 + 32;
 }
 
-double ktoc(double k) {
+double ftoc(double f) {
 
-  if (k < 0)
-    throw runtime_error("Temperature is below 0 K");
+  if (f < -459.67)
+    throw runtime_error("Temperature is below -459.67 F");
 
-  return k - 273.15;
+  return f - 32  1.8);
 }
 
 pair<double, char> conversion(char unit, double temp) {
@@ -24,13 +24,13 @@ pair<double, char> conversion(char unit, double temp) {
   switch (unit) {
   case 'C':
   case 'c':
-    unit = 'K';
-    return make_pair(ctok(temp), unit);
+    unit = 'F';
+    return make_pair(ctof(temp), unit);
 
-  case 'K':
-  case 'k':
+  case 'F':
+  case 'f':
     unit = 'C';
-    return make_pair(ktoc(temp), unit);
+    return make_pair(ftoc(temp), unit);
 
   default:
     throw runtime_error("Invalid unit");
@@ -40,7 +40,7 @@ pair<double, char> conversion(char unit, double temp) {
 int main() {
   double temp{};
   char unit{};
-  cout << "Enter temperature and unit (C/K): ";
+  cout << "Enter temperature and unit (C/F): ";
   cin >> temp >> unit;
 
   if (!cin) {
@@ -58,4 +58,6 @@ int main() {
   }
 }
 
-// Add to the program so that it can also convert from Kelvin to Celsius.
+// Write a program that converts from Celsius to Fahrenheit and from
+// Fahrenheit to Celsius. Use estimation (§4.7.1) to see if your results are
+// plausible.
