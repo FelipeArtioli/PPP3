b261d2777699e27132b9e786e445e507e8500028
Felipe Artioli <feartioli.br@gmail.com>
2025-09-18 19:07:28 -0300

    Comment ppp_3_13.cpp
    Comment exercise 13 of chapter 3.

--- Chapter_3/ppp_3_13.cpp
+++ Chapter_3/ppp_3_13.cpp
@@ -1,15 +1,19 @@
 // PPP - Chapter 3 - Exercise 13
+// Sieve of Eratosthenes
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
+// Vector to store found prime numbers
 vector<int> primes = {};
 
+// Generates primes up to 'range' using the sieve algorithm
 void prime_generator(int range) {
   vector<bool> is_prime(range + 1, true);
   is_prime[0] = false;
   is_prime[1] = false;
+  // Mark multiples of each prime as non-prime
   for (int i = 2; i * i <= range; ++i) {
     if (is_prime[i]) {
       for (int j = i * i; j <= range; j += i) {
@@ -17,6 +21,8 @@ void prime_generator(int range) {
       }
     }
   }
+
+  // Collect primes into the primes vector
   for (int i = 2; i < (range + 1); ++i) {
     if (is_prime[i]) {
       primes.push_back(i);
@@ -24,6 +30,7 @@ void prime_generator(int range) {
   }
 }
 
+// Prints all primes up to 'range'
 void print_primes(int range) {
   cout << "List of primes from 2 to " << range << ":\n";
   for (int i = 0; i < size(primes); ++i) {
@@ -36,9 +43,9 @@ void print_primes(int range) {
 }
 
 int main() {
-  int range{100};
-  prime_generator(range);
-  print_primes(range);
+  int range{100};         // Max number to check
+  prime_generator(range); // Generate primes
+  print_primes(range);    // Display primes
 }
 
 // Create a program to find all the prime numbers between 1 and 100.
