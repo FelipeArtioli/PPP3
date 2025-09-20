7ca007722e3348019f8a6919f957b2bcc297eedb
Felipe Artioli <feartioli.br@gmail.com>
2025-09-19 23:02:04 -0300

    Comment ppp_3_16.cpp

--- Chapter_3/ppp_3_16.cpp
+++ Chapter_3/ppp_3_16.cpp
@@ -1,4 +1,5 @@
 // PPP - Chapter 3 - 16
+// Find min, max, and mode of a set of strings
 
 #include <algorithm>
 #include <iostream>
@@ -6,11 +7,13 @@
 
 using namespace std;
 
+// Sort words alphabetically
 vector<string> sort_words(vector<string> words) {
   sort(words.begin(), words.end());
   return words;
 }
 
+// Find the mode
 string find_mode(vector<string> words) {
   int curr_count{1}, max_count{1};
   string mode{};
@@ -36,6 +39,7 @@ int main() {
   string input{};
   cout << "Enter a set of strings to find the min, max and mode of the set:\n";
 
+  // Read words
   while (cin >> input) {
     words.push_back(input);
   }
@@ -45,11 +49,12 @@ int main() {
     return 0;
   }
 
+  // Sort and find mode
   words = sort_words(words);
   string mode = find_mode(words);
   string min = words[0];
   string max = words[words.size() - 1];
-
+  // Print results
   cout << "Max = " << max << '\n';
   cout << "Min = " << min << '\n';
   if (mode.empty())
