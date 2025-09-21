97360709cc45882ec56914e95287f539ae105b68
Felipe Artioli <feartioli.br@gmail.com>
2025-09-20 23:25:07 -0300

    Comment ppp_3_20.cpp

--- Chapter_3/ppp_3_20.cpp
+++ Chapter_3/ppp_3_20.cpp
@@ -1,4 +1,5 @@
 // PPP - Chapter 3 - 20
+// Score to name lookup
 
 #include <algorithm>
 #include <iostream>
@@ -6,57 +7,64 @@
 
 using namespace std;
 
+// Check if any name was entered more than once
 bool check_repetition(vector<string> names) {
 
-  sort(names.begin(), names.end());
+  sort(names.begin(), names.end()); // Sort names alphabetically
   for (int i = 1; i < names.size(); ++i) {
-    if (names[i] == names[i - 1]) {
+    if (names[i] == names[i - 1]) { // Duplicate found
       cout << "ERROR: The name '" << names[i] << "' was entered twice.\n";
       return true;
     }
   }
-  return false;
+  return false; // No duplicates
 }
-
+// Find and print all names associated with a given score
 void find_name(vector<string> names, vector<int> scores) {
 
   int score{};
-  while (cin >> score) {
-    if (score == -1)
+  while (cin >> score) { // Read scores from user
+    if (score == -1)     // Sentinel to stop querying
       break;
     bool found = false;
+
+    // Search for all names matching the score
     for (int i = 0; i < names.size(); ++i) {
       if (score == scores[i]) {
         cout << names[i] << '\n';
         found = true;
       }
     }
-    if (!found)
+    if (!found) // No names matched
       cout << "Score not found.\n";
   }
 }
 
 int main() {
 
-  vector<string> names = {};
-  string input_name{};
+  vector<string> names = {}; // Store entered names
+  string input_name{};       // Store corresponding scores
   vector<int> scores = {};
   int input_score{};
   cout << "Enter a set of name-and-value pairs, such as 'Joe 17'. Terminate "
           "input with 'NoName 0':\n";
 
+  // Read name-score pairs from the user
   while (cin >> input_name >> input_score) {
-    if (input_name == "NoName" && input_score == 0)
+    if (input_name == "NoName" && input_score == 0) // Sentinel to stop input
       break;
     names.push_back(input_name);
     scores.push_back(input_score);
   }
 
+  // Check for duplicate names
   if (check_repetition(names))
     return 0;
 
   cout << "Enter a score to find names with that score.\n"
        << "Enter '-1' to quit the program.\n";
+
+  // Allow user to query all names matching a score
   find_name(names, scores);
 
   return 0;
