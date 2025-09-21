a2ce185801164103f8042e1ab7575e6ecbb6e842
Felipe Artioli <feartioli.br@gmail.com>
2025-09-20 23:13:43 -0300

    Comment ppp_3_18.cpp

--- Chapter_3/ppp_3_18.cpp
+++ Chapter_3/ppp_3_18.cpp
@@ -1,4 +1,5 @@
 // PPP - Chapter 3 - 18
+// Name and Score Recorder
 
 #include <algorithm>
 #include <iostream>
@@ -6,6 +7,7 @@
 
 using namespace std;
 
+// Print all names and their corresponding scores
 void print(vector<string> names, vector<int> scores) {
   for (int i = 0; i < names.size(); ++i) {
     cout << names[i] << ": " << scores[i] << '\n';
@@ -13,25 +15,28 @@ void print(vector<string> names, vector<int> scores) {
   cout << '\n';
 }
 
+// Check if any name was entered more than once
 bool check_repetition(vector<string> names) {
-  sort(names.begin(), names.end());
+  sort(names.begin(), names.end()); // Sort names alphabetically
   for (int i = 1; i < names.size(); ++i) {
-    if (names[i] == names[i - 1]) {
+    if (names[i] == names[i - 1]) { // Duplicate found
       cout << "ERROR: The name '" << names[i] << "' was entered twice.\n";
       return true;
     }
   }
-  return false;
+  return false; // No duplicates
 }
 
 int main() {
 
-  vector<string> names = {};
-  string input_name{};
+  vector<string> names = {}; // Store entered names
+  string input_name{};       // Store corresponding scores
   vector<int> scores = {};
   int input_score{};
   cout << "Enter a set of name-and-value pairs, such as 'Joe 17'. Terminate "
           "input with 'NoName 0':\n";
+
+  // Read name-score pairs from the user
   while (cin >> input_name >> input_score) {
     if (input_name == "NoName" && input_score == 0)
       break;
@@ -39,9 +44,11 @@ int main() {
     scores.push_back(input_score);
   }
 
+  // Check for duplicate names
   if (check_repetition(names))
     return 0;
 
+  // Print all collected names and scores
   cout << '\n';
   print(names, scores);
 }
